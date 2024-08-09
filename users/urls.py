33cf935ed6940c83bb80257cf91b7c3480350168
marcos <marcos@gmail.com>
2024-08-08 22:55:05 -0300

    erro na tela de registro no momento do redirecionamento

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, logoutDashboard
+from .views import home, logoutDashboard, SignupView
 
 urlpatterns = [  
     path('home/', home.as_view(), name='home'),
-    path("logout/", logoutDashboard.as_view(), name="logout")
+    path("logout/", logoutDashboard.as_view(), name="logout"),
+    path("signup/", SignupView.as_view(), name="signup")
 ] 
 
--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
 from django.views.generic.base import TemplateView
+from allauth.account.forms import SignupForm
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
 
 class home(TemplateView):
     template_name='base.html'
 
+
 class logoutDashboard(TemplateView):
     template_name = 'account/logout.html'
 
+
+class SignupView(FormView):
+    template_name = 'account/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('account_login')  # URL para redirecionar após sucesso
+
+    def form_valid(self, form):
+        form.save(self.request)  # Salva o usuário no banco de dados
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
